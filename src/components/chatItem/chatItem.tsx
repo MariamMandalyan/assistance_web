84c514a55653a15352519f64f52acf5993b46c9a
Mariam <mar.mandalyan@gmail.com>
2025-01-09 14:12:44 +0700

    changes

--- src/components/chatItem/chatItem.tsx
+++ src/components/chatItem/chatItem.tsx
@@ -10,6 +10,7 @@ interface IProps {
 }
 
 const ChatItem: React.FC<IProps> = ({ message }) => {
+  console.log(message, ',,,,,,');
   const content = useMemo(() => {
     // ${message.type === 'link' && styles.link}`}
     let lawyer = message?.type === 'lawyer'
@@ -23,45 +24,36 @@ const ChatItem: React.FC<IProps> = ({ message }) => {
       )
     } else {
       return (
-        <>
-          {
-            message?.summary && <div className={styles.container} style={{ justifyContent: 'flex-start' }}>
-              <div className={`${styles.content} ${styles.answer} ${lawyer && styles.lawyer}`}>
-                {lawyer && <div className={styles.circle}>
-                  <img src={lawyerIcon} />
-                </div>}
-                <p className={styles.text} >{message.summary}</p>
-              </div>
-            </div>
-          }
-          {
-            message?.details_answer && <div className={styles.container} style={{ justifyContent: 'flex-start' }}>
-              <div className={`${styles.content} ${styles.answer}  ${lawyer && styles.lawyer}`}>
-                {lawyer && <div className={styles.circle}>
-                  <img src={lawyerIcon} />
-                </div>}
-                <p className={styles.text} dangerouslySetInnerHTML={{ __html: message.details_answer }}  >
-                </p>
-              </div>
-            </div>
-          }
-          {
-            message?.additional_sources && message.additional_sources.map((item: string, index: number) => {
-              return (
-                <div className={styles.container} style={{ justifyContent: 'flex-start' }} key={index}>
-                  <div className={`${styles.content} ${styles.answer}`}>
-                    {lawyer && <div className={`${styles.circle} ${styles.linkCircle}`}>
-                      <img src={lawyerIcon} />
-                    </div>}
-                    <a className={`${styles.text} ${styles.link}`} href={item} target={'_blank'}>
-                      {`Ссылка ${message?.additional_sources?.length > 1 ? index + 1 : ''}`}
+        <div className={styles.container} style={{ justifyContent: 'flex-start' }}>
+          <div className={`${styles.content} ${styles.answer} ${lawyer && styles.lawyer}`}>
+            {lawyer && <div className={styles.circle}>
+              <img src={lawyerIcon} />
+            </div>}
+            {
+              message?.summary &&
+              <p className={styles.text}>
+                {message.summary}
+              </p>
+            }
+            {
+              message?.details_answer &&
+              <p className={styles.text}
+                dangerouslySetInnerHTML={{ __html: message.details_answer }}  >
+              </p>
+            }
+            {
+              message?.additional_sources && <div>{message?.additional_sources.map((item: string, index: number) => {
+                return (
+                    <a className={`${styles.text} ${styles.link}`} href={item} target={'_blank'} key={index}>
+                      {message.legal_references[index]}
+                      {/* {`Ссылка ${message?.additional_sources?.length > 1 ? index + 1 : ''}`} */}
                     </a>
-                  </div>
-                </div>
-              )
-            }, [])
-          }
-        </>
+                )
+              })}
+              </div>
+            }
+          </div>
+        </div>
       )
 
     }
